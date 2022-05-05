a1ebb5daff499469ebcd6c1d611ad357f2f20b6f
Thib-numberX <peronnothibault@hotmail.fr>
2022-05-05 16:14:28 +0200

    fix error increment to increments on bookmarks migration file

--- laravel/database/migrations/2022_05_05_125130_create_bookmarks_table.php
+++ laravel/database/migrations/2022_05_05_125130_create_bookmarks_table.php
@@ -16,7 +16,7 @@ class CreateBookmarksTable extends Migration
         Schema::create('bookmarks', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->collation = 'utf8mb4_general_ci';
-            $table->increment('id')->unsigned();
+            $table->increments('id')->unsigned();
             $table->bigInteger('id_spot',10)->unsigned();
             $table->foreign('id_spot')->references('id')->on('spots');
             $table->bigInteger('id_comment',10)->unsigned();
