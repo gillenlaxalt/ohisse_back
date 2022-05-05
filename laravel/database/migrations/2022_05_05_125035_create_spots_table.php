df02ee75d515ef29cdc97522f90a5dbaf5043670
Thib-numberX <peronnothibault@hotmail.fr>
2022-05-05 15:15:48 +0200

    completed spots s table migration file

--- laravel/database/migrations/2022_05_05_125035_create_spots_table.php
+++ laravel/database/migrations/2022_05_05_125035_create_spots_table.php
@@ -14,7 +14,23 @@ class CreateSpotsTable extends Migration
     public function up()
     {
         Schema::create('spots', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id')->unsigned();
+            $table->string('name', 64);
+            $table->integer('number', 10)->unsigned() ->nullable();
+            $table->string('street',100)->unsigned() ->nullable();
+            $table->int('zipcode',10)->unsigned() ->nullable();
+            $table->string('city', 64);
+            $table->string('country', 64);
+            $table->string('longitude', 64);
+            $table->string('latitude', 64);
+            $table->string('discipline', 64);
+            $table->string('type', 64);
+            $table->string('rock_type', 64)->nullable();
+            $table->string('picture', 255)->nullable();
+            $table->string('various', 255);
+            $table->integer('reputation')->nullable();
+            $table->string('min_difficulty', 64)->nullable();
+            $table->string('max_difficulty', 64)->nullable();
             $table->timestamps();
         });
     }
