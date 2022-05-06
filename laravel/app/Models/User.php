f7e9164c68a4f86296305a575864758f12f5c0e6
FredericHavas <ultimasluces@gmail.com>
2022-05-06 14:51:05 +0200

    created user model and user controller

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -1,49 +1,13 @@
 <?php
 
-namespace App\Models;
+    // On use le "CoreModel" de Eloquent pour en hériter
+  // et ainsi avoir tout plein de trucs déjà codés !
+  namespace App\Models;
+  use Illuminate\Database\Eloquent\Model;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+  class User extends Model
+  {
+    // Par défaut, Eloquent va chercher une table qui porte le nom
+    // du modèle au pluriel, ici 'users'
 
-class User extends Authenticatable
-{
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'firstname',
-        'lastname',
-        'pseudo',
-        'city',
-        'country',
-        'description',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-}
+  }
