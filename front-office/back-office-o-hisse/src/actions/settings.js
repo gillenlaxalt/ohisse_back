7b45a808f5b5df84d65b2cc17a47f4322232c13f
Thib-numberX <peronnothibault@hotmail.fr>
2022-05-10 16:18:15 +0200

    delete files back et front office

--- front-office/back-office-o-hisse/src/actions/settings.js
+++
@@ -1,9 +0,0 @@
-export const HANDLE_MENU = 'HANDLE_MENU';
-export const handleMenu = () => ({
-  type: HANDLE_MENU,
-})
-
-export const HANDLE_DARK = 'HANDLE_DARK';
-export const handleDark = () => ({
-  type: HANDLE_DARK,
-})
