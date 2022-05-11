65913c43d296818fee17069480bade929c696716
Thib-numberX <peronnothibault@hotmail.fr>
2022-05-11 09:20:13 +0200

    create div component to cheat with the header fixed

--- front/src/components/login/Login.js
+++ front/src/components/login/Login.js
@@ -5,25 +5,38 @@
 // local
 
 // style
+import './login.scss';
 
 function Login() {
   return (
     <section className="login">
       <h2 className="login_h2">Bienvenue</h2>
       <p className="login_txt">Bonjour, si tu es ici c’est que tu as le rôle d’admin sur O’hisee</p>
-      <form>
-        <label htmlFor="email">E-mail</label>
+      <form className="login_form">
+        <label
+        htmlFor="email"
+        className="login-form_label"
+        >E-mail</label>
         <input
         id='email'
         type='email'
-        placeholder="Rentre ton e-mail"
+        placeholder="Ton e-mail"
+        className="login-form_input"
         />
-        <label htmlFor="Password">Password</label>
+        <label
+        htmlFor="Password"
+        className="login-form_label"
+        >Password</label>
         <input
         id='Password'
         type='Password'
-        placeholder="ton mot de passe"
+        placeholder="Ton mot de passe"
+        className="login-form_input"
         />
+        <button
+        type='submit'
+        className="login-form_btn"
+        >connexion</button>
       </form>
 
     </section>
