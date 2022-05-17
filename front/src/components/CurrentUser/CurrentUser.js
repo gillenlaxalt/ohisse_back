55478352990720132e01657077db9c0a5db38c3c
Thib-numberX <peronnothibault@hotmail.fr>
2022-05-17 17:13:02 +0200

    ask by git

--- front/src/components/CurrentUser/CurrentUser.js
+++ front/src/components/CurrentUser/CurrentUser.js
@@ -31,12 +31,12 @@ function CurrentUser() {
             </span>
           </div>
           <div className='profil-form_info'>
-              <label id='city'>Ville</label>
-              <select htmlFor='city'>
-                <option value='role'>role</option>
-                <option value='admin'>admin</option>
-                <option value='user'>user</option>
-              </select>
+            <label id='city'>Ville</label>
+            <select htmlFor='city'>
+              <option value='role'>role</option>
+              <option value='admin'>admin</option>
+              <option value='user'>user</option>
+            </select>
           </div>
           <div className='profil-form_descrip'>
             <label htmlFor='decrip'>Description</label>
@@ -44,38 +44,30 @@ function CurrentUser() {
           </div>
           <div className='profil-form_btn'>
             <button
-            type='submit'
-            className="profil-form_update profil-form-btn"
+              type='submit'
+              className="profil-form_update profil-form-btn"
             >Modifier</button>
             <button
-            type='submit'
-            className="profil-form_delete profil-form-btn"
+              type='submit'
+              className="profil-form_delete profil-form-btn"
             >Supprimer</button>
           </div>
         </form>
 
       </section>
+      
       <section className='current-user_map'>
-      <div className="full-map">
-      <MapContainer center={[46.7, 2]} zoom={6} scrollWheelZoom>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {/* <Marker key={item.id} position={[item.longitude, item.latitude]} icon={ohisseIcon}>
+        <MapContainer center={[47, 2]} zoom={5} scrollWheelZoom>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[47, 2]}>
             <Popup>
-              <div className="map-popup" style={popupContent}>
-                <Link to={`/fiche-spot/${item.name}`}>
-                  <img src={item.picture ? item.picture : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShhshYwTK1ZujWFb4UJzeLDzywVv6UgWowhA&usqp=CAU'} alt={`${item.name}`} />
-                  <h3 style={popupHead}>{item.name}</h3>
-                </Link>
-                {item.type} <br />
-                {item.discipline}
-              </div>
+              A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
-          </Marker> */}
-      </MapContainer>
-    </div>
+          </Marker>
+        </MapContainer>
       </section>
     </section>
   )
