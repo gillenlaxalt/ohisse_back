4d2bfa1c512ca6ab88b1c8d13d279dee78028a65
Thib-numberX <peronnothibault@hotmail.fr>
2022-05-18 08:03:11 +0200

    up to date footer with a div ta cheat with footer fixed

---
+++ front/src/components/footer/Div.js
@@ -0,0 +1,19 @@
+//== import 
+
+// npm
+
+// local
+
+// style
+import './div.scss'
+
+function Div() {
+  return(
+    <div className="footer-div">
+      
+    </div>
+
+  )
+};
+
+export default Div
