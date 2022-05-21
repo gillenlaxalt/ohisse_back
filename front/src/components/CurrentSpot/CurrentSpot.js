e53cbb74d82eff7c3951b232f03c14a43fe5ac19
Thib-numberX <peronnothibault@hotmail.fr>
2022-05-21 11:20:45 +0200

    added description text area input and this scss

--- front/src/components/CurrentSpot/CurrentSpot.js
+++ front/src/components/CurrentSpot/CurrentSpot.js
@@ -14,31 +14,47 @@ function CurrentSpot() {
         <h1 className='currentSpot-info_h1'>Nom du spot</h1>
         <form className='currentSpot-info_form'>
           <div className='info-form_img-url form_flex-div'>
-            <img src='' alt='' className='info-form-img' />
-            <label htmlFor='url'>URL de l'image</label>
-            <input type='url' name='url'></input>
-            <label htmlFor='spot' className='info-form-url'>Nom du spot</label>
-            <input type='text' name='spot'></input>
+            <img src='https://img.lemde.fr/2021/10/22/0/0/6016/4016/664/0/75/0/e2b0bed_673149149-arkose-didot-3-arkose-3.jpg' alt='' className='info-form-img' />
+            <span className='info-form_url'>
+              <label htmlFor='spot' className='info-form-url'>Nom du spot</label>
+              <input type='text' name='spot'></input>
+              <label htmlFor='url'>URL de l'image</label>
+              <input type='url' name='url'></input>
+            </span>
           </div>
           <div className='info-form_num-street form_flex-div'>
-            <label htmlFor='number' className='info-form-num'>Numéro</label>
-            <input type='text' name='number'></input>
-            <label htmlFor='street' className='info-form-street'>Rue</label>
-            <input type='text' name='street'></input>
+            <span className='form_flex-span flex-span-margin-right'>
+              <label htmlFor='number' className='info-form-num'>Numéro</label>
+              <input type='text' name='number'></input>
+            </span>
+            <span className='info-form-street form_flex-span'>
+              <label htmlFor='street' className='info-form-street'>Rue</label>
+              <input type='text' name='street'></input>
+            </span>
           </div>
           <div className='info-form_zip-city form_flex-div'>
-            <label htmlFor='zipcode' className='info-form-zip'>Code Postal</label>
-            <input type='number' name='zipcode'></input>
-            <label htmlFor='city' className='info-form-city'>Ville</label>
-            <input type='text' name='city'></input>
+            <span className='form_flex-span flex-span-margin-right'>
+              <label htmlFor='zipcode' className='info-form-zip'>Code Postal</label>
+              <input type='number' name='zipcode'></input>
+            </span>
+            <span className='form_flex-span'>
+              <label htmlFor='city' className='info-form-city'>Ville</label>
+              <input type='text' name='city'></input>
+            </span>
           </div>
           <div className='info-form_country-long-lat form_flex-div'>
-            <label htmlFor='country'className='info-form-country'>Pays</label>
-            <input type='text' name='country'></input>
-            <label htmlFor='longitude' className='info-form-long'>Longitude</label>
-            <input type='text' name='longitude'></input>
-            <label htmlFor='latitude' className='info-form-lat'>Latitude</label>
-            <input type='text' name='latitude'></input>
+            <span className='form_flex-span flex-span-margin-right'>
+              <label htmlFor='country'className='info-form-country'>Pays</label>
+              <input type='text' name='country'></input>
+            </span>
+            <span className='form_flex-span flex-span-margin-right'>
+              <label htmlFor='longitude' className='info-form-long'>Longitude</label>
+              <input type='text' name='longitude'></input>
+            </span>
+            <span className='form_flex-span'>
+              <label htmlFor='latitude' className='info-form-lat'>Latitude</label>
+              <input type='text' name='latitude'></input>
+            </span>
           </div>
           <div className='info-form_disc-type-rep form_flex-div'>
             <label htmlFor='discipline'>Discipline</label>
@@ -64,13 +80,16 @@ function CurrentSpot() {
             </select>
           </div>
           <div className='info-form_diffMin-diffMax form_flex-div'>
-            <label htmlFor='minDiff'>Difficulté minimale</label>
-              <select name='minDiff'>
-                <option value="">minDiff</option>
-                <option value='mid1'>mid1</option>
-                <option value='mid2'>mid2</option>
-                <option value='mid3'>mid3</option>
-              </select>
+            <span className='form_flex-span flex-span-margin-right'>
+              <label htmlFor='minDiff'>Difficulté minimale</label>
+                <select name='minDiff'>
+                  <option value="">minDiff</option>
+                  <option value='mid1'>mid1</option>
+                  <option value='mid2'>mid2</option>
+                  <option value='mid3'>mid3</option>
+                </select>
+            </span>
+            <span className='form_flex-span'>
               <label htmlFor='maxDiff'>Difficulté maximale</label>
               <select name='maxDiff'>
                 <option value="">maxDiff</option>
@@ -78,7 +97,20 @@ function CurrentSpot() {
                 <option value='mad2'>mad2</option>
                 <option value='mad3'>mad3</option>
               </select>
+            </span>
           </div>
+          <div className='spot-form_descrip'>
+
+              <label htmlFor='decrip'>Description</label>
+              <input
+                type='textarea'
+                name='descrip'
+                // value={description}
+                // defaultValue={userById.description}
+                // onChange={(evt) => handleChangeInput(evt.target.value, 'description')}
+              ></input>
+
+            </div>
           <div className='spot-form_btn'>
               <button
                 // onClick={handleUpdateSubmit}
